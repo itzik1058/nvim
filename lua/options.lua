3f8e7c0d5282b824382488fd6dd44b217e46c35f
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-22 14:58:28 +0200

    sync with system clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -8,7 +8,7 @@ vim.o.hlsearch = false
 vim.wo.number = true
 
 -- sync clipboard
--- vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 vim.o.breakindent = true
 vim.o.undofile = true
@@ -16,11 +16,11 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 vim.o.termguicolors = true
