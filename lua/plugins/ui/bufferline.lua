19e6bd566c79ae5e9fc5ea102fd43e4b8a83dca4
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-22 15:01:16 +0200

    disable bufferline

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,7 +1,7 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  "akinsho/bufferline.nvim",
+  enabled = false,
+  dependencies = "nvim-tree/nvim-web-devicons",
   opts = {
     options = {
       diagnostics = "nvim_lsp",
@@ -10,20 +10,15 @@ return {
           filetype = "NvimTree",
           text = "NvimTree",
           text_align = "left",
-          separator = true
-        }
+          separator = true,
+        },
       },
-    }
+    },
   },
   config = function(_, opts)
-    require('bufferline').setup(opts)
+    require("bufferline").setup(opts)
 
-    vim.keymap.set('n', '<tab>', '<cmd> BufferLineCycleNext <CR>', { desc = 'Next buffer' })
-    vim.keymap.set('n', '<S-tab>', '<cmd> BufferLineCyclePrev <CR>', { desc = 'Previous buffer' })
-    vim.keymap.set('n', '<leader>x', function()
-      vim.cmd("write")
-      require('bufferline').unpin_and_close()
-      vim.cmd("BufferLineCycleNext")
-    end, { desc = 'Close buffer' })
-  end
+    vim.keymap.set("n", "<tab>", "<cmd> BufferLineCycleNext <CR>", { desc = "Next buffer" })
+    vim.keymap.set("n", "<S-tab>", "<cmd> BufferLineCyclePrev <CR>", { desc = "Previous buffer" })
+  end,
 }
