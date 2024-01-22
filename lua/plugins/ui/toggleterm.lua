e6987a3c7a6a4c24bdf86fbffafd539dc6b98626
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-22 14:57:58 +0200

    toggleterm floating window

--- lua/plugins/ui/toggleterm.lua
+++ lua/plugins/ui/toggleterm.lua
@@ -1,12 +1,13 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {},
+  opts = {
+    direction = "float",
+  },
   config = function(_, opts)
-    local toggleterm = require("toggleterm")
+    local toggleterm = require "toggleterm"
     toggleterm.setup(opts)
 
     vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
-    vim.keymap.set("t", "<Esc>", "<cmd>ToggleTerm<CR>")
-  end
+  end,
 }
