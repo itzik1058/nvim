a2ccf38aa5f1f3dd8c7d3367c2240ee5362c884a
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-22 14:56:31 +0200

    open nvim-tree in center

--- lua/global.lua
+++ lua/global.lua
@@ -1,1 +1,5 @@
 vim.g.mapleader = " "
+
+-- disable netrw in favor of nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- lua/plugins/ui/tree.lua
+++ lua/plugins/ui/tree.lua
@@ -1,3 +1,25 @@
+local WIDTH_RATIO = 0.5
+local HEIGHT_RATIO = 0.8
+
+local open_win_config = function()
+  local screen_w = vim.opt.columns:get()
+  local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+  local window_w = screen_w * WIDTH_RATIO
+  local window_h = screen_h * HEIGHT_RATIO
+  local window_w_int = math.floor(window_w)
+  local window_h_int = math.floor(window_h)
+  local center_x = (screen_w - window_w) / 2
+  local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+  return {
+    border = "rounded",
+    relative = "editor",
+    row = center_y,
+    col = center_x,
+    width = window_w_int,
+    height = window_h_int,
+  }
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -6,80 +28,25 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
-    disable_netrw = false,
+    disable_netrw = true,
     hijack_netrw = true,
-    hijack_cursor = true,
-    hijack_unnamed_buffer_when_opening = false,
     sync_root_with_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_root = false,
-    },
+    respect_buf_cwd = true,
     view = {
-      adaptive_size = false,
-      side = "left",
-      width = 30,
-      preserve_window_proportions = true,
-    },
-    git = {
-      enable = false,
-      ignore = true,
-    },
-    filesystem_watchers = {
-      enable = true,
-    },
-    actions = {
-      open_file = {
-        resize_window = true,
+      relativenumber = true,
+      float = {
+        enable = true,
+        open_win_config = open_win_config,
       },
-    },
-    renderer = {
-      root_folder_label = false,
-      highlight_git = false,
-      highlight_opened_files = "none",
-
-      indent_markers = {
-        enable = false,
-      },
-
-      icons = {
-        show = {
-          file = true,
-          folder = true,
-          folder_arrow = true,
-          git = false,
-        },
-
-        glyphs = {
-          default = "󰈚",
-          symlink = "",
-          folder = {
-            default = "",
-            empty = "",
-            empty_open = "",
-            open = "",
-            symlink = "",
-            symlink_open = "",
-            arrow_open = "",
-            arrow_closed = "",
-          },
-          git = {
-            unstaged = "✗",
-            staged = "✓",
-            unmerged = "",
-            renamed = "➜",
-            untracked = "★",
-            deleted = "",
-            ignored = "◌",
-          },
-        },
-      },
+      width = function()
+        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+      end,
     },
   },
-  config = function (_, opts)
-    local nvim_tree = require("nvim-tree")
+  config = function(_, opts)
+    local nvim_tree = require "nvim-tree"
     nvim_tree.setup(opts)
-    vim.keymap.set('n', '<C-n>', '<cmd> NvimTreeToggle <CR>', { desc = "Toggle nvimtree" })
-    vim.keymap.set('n', '<leader>e', '<cmd> NvimTreeFocus <CR>', { desc = "Focus nvimtree" })
-  end
+    vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvimtree" })
+    vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeFocus <CR>", { desc = "Focus nvimtree" })
+  end,
 }
