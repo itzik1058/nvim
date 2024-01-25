0b265111116c5b2a0edea5fe4e801aae2ef67bf1
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-25 11:42:39 +0200

    update and fix treesitter config

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -1,8 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
   },
   opts = {
     ensure_installed = {
@@ -23,25 +25,33 @@ return {
     },
     auto_install = true,
 
-    highlight = { enable = true },
-    indent = { enable = true },
+    highlight = {
+      enable = true,
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
+    indent = { enable = false },
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "<c-space>",
-        node_incremental = "<c-space>",
-        scope_incremental = "<c-s>",
-        node_decremental = "<M-space>",
+        node_incremental = "v",
       },
     },
     textobjects = {
       select = {
         enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        -- lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
           ["aa"] = "@parameter.outer",
           ["ia"] = "@parameter.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
           ["af"] = "@function.outer",
           ["if"] = "@function.inner",
           ["ac"] = "@class.outer",
@@ -52,29 +62,33 @@ return {
         enable = true,
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
         },
         goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
         },
         goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
         },
         goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
         },
       },
       swap = {
         enable = true,
         swap_next = {
-          ["<leader>a"] = "@parameter.inner",
+          ["<leader>csp"] = "@parameter.inner",
+          ["<leader>csf"] = "@function.outer",
+          ["<leader>csc"] = "@class.outer",
         },
         swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
+          ["<leader>csP"] = "@parameter.inner",
+          ["<leader>csF"] = "@function.outer",
+          ["<leader>csC"] = "@class.outer",
         },
       },
     },
