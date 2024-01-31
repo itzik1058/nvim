4cc55de191797d9d01bf05769371fd8f1aa6af60
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-01-31 10:21:35 +0200

    nvim-tree update focused file

--- lua/plugins/ui/tree.lua
+++ lua/plugins/ui/tree.lua
@@ -48,6 +48,10 @@ return {
     hijack_netrw = true,
     sync_root_with_cwd = true,
     respect_buf_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+    },
     view = {
       relativenumber = true,
       float = {
